1307805dd0e8085c533c40c9205a075976abfb5a
Manuel Soto Romero <manu@ciencias.unam.mx>
2019-03-17 19:21:26 -0600

    Ejemplo 1 :: Ejemplo 2.5

--- ejemplo01/altos.pl
+++ ejemplo01/altos.pl
@@ -8,3 +8,7 @@ alto(ricardo,manuel).
 alto(javier,ricardo).
 alto(alejandro,adriana).
 alto(javier,alejandro).
+
+% Predicado que indica si una persona X es más alta que otra Y.
+mas_alto(X,Y) :- alto(X,Y). 
+mas_alto(X,Y) :- alto(X,Z), alto(Z,Y).
