abaa3222ed898c363c833805d88ab6daf53c0da6
Manuel Soto Romero <manu@ciencias.unam.mx>
2019-03-17 22:59:57 -0600

    Ejemplos 1 :: Ejemplo 2.6

--- ejemplos01/listas.pl
+++ ejemplos01/listas.pl
@@ -3,7 +3,11 @@
 contiene(E,[E|_]).
 contiene(E,[_|R]) :- contiene(E,R).
 
-% Predicado concatena(X,Y,Z) que indica que representa a Z como la 
+% Predicado concatena(X,Y,Z) que representa a Z como la 
 % concatenación de las listas X y Y.
 concatena([],L2,L2).
 concatena([C1|R1],L2,[C1|R3]) :- concatena(R1,L2,R3).
+
+% Predicado concatenaDif1(X,Y,Z) que representa a Z como la 
+% concatenación de las listas X y Y.
+concatenaDif1(A-Ha,B-Hb,C-Hc) :- Ha = B, Hb = Hc, C = A.
