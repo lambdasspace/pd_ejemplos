24dacdd2b9fff846ef4831359b133671bd69e284
Manuel Soto Romero <manu@ciencias.unam.mx>
2019-03-17 20:20:56 -0600

    Ejemplos 1 :: Ejemplo 2.8

--- ejemplo01/listas.pl
+++ ejemplo01/listas.pl
@@ -2,3 +2,8 @@
 % contenido en la lista L.
 contiene(E,[E|_]).
 contiene(E,[_|R]) :- contiene(E,R).
+
+% Regla que se cumple si una lista es el resultado de concatenar
+% otras dos.
+concatena([],L2,L2).
+concatena([C1|R1],L2,[C1|R3]) :- concatena(R1,L2,R3).
