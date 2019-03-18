2f63adf9d47c56c8d9e088586129abd94ad35e0c
Manuel Soto Romero <manu@ciencias.unam.mx>
2019-03-18 14:33:02 -0600

    Ejemplos 2 :: Actualiza Factorial

--- ejemplos02/factorial.pl
+++ ejemplos02/factorial.pl
@@ -1,7 +1,7 @@
-% Regla que se cumple si F es el factorial de X.
+% Predicado factorial(N,F) que relaciona un número N con su respectivo 
+% factorial F.
 factorial(0,1) :- !.
 factorial(X,F) :-
-    X2 is X-1,
-    factorial(X2,F2),
-    F is X*F2.
-    
+   X2 is X-1,
+   factorial(X2,F2),
+   F is X*F2.
