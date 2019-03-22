0012268e4a8bf695ae68d91d5b2c14a2c2af138b
Manuel Soto Romero <manu@ciencias.unam.mx>
2019-03-22 00:45:53 -0600

    Ejemplos 3 :: Valor absoluto 2

--- ejemplos03/Ejemplo.hs
+++ ejemplos03/Ejemplo.hs
@@ -26,7 +26,7 @@ module Ejemplo where
    absoluto x = if x >= 0 then x else x * (-1)
 
    -- Función que calcula el valor absoluto de un número.
-   absoluto :: Int -> Int
-   absoluto x
+   absoluto2 :: Int -> Int
+   absoluto2 x
       | x >= 0 = x
       | x < 0 = x * (-1)      
