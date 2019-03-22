a8e1f5dc9f68b47fb149b4dad8e40f41bd7fddd2
Manuel Soto Romero <manu@ciencias.unam.mx>
2019-03-22 00:50:04 -0600

    Ejemplos 3 :: Mes

--- ejemplos03/Ejemplo.hs
+++ ejemplos03/Ejemplo.hs
@@ -29,4 +29,21 @@ module Ejemplo where
    absoluto2 :: Int -> Int
    absoluto2 x
       | x >= 0 = x
-      | x < 0 = x * (-1)      
+      | x < 0 = x * (-1)
+
+   -- Función que obtiene el nombre de un mes dado su número.
+   mes :: Int -> String
+   mes n
+      | n == 1 = "Enero"
+      | n == 2 = "Febrero"
+      | n == 3 = "Marzo"
+      | n == 4 = "Abril"
+      | n == 5 = "Mayo"
+      | n == 6 = "Junio"
+      | n == 7 = "Julio"
+      | n == 8 = "Agosto"
+      | n == 9 = "Septiembre"
+      | n == 10 = "Octubre"
+      | n == 11 = "Noviembre"
+      | n == 12 = "Diciembre"
+      | otherwise = error "Número inválido"          
